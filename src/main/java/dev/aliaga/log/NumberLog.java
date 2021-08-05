29c50f8ac55d3ed286fb0ad996d4585f38e07a23
Mario Aliaga <mario@aliaga.dev>
2021-08-05 16:28:16 -0400

    Add Friends Numbers

--- src/main/java/dev/aliaga/log/NumberLog.java
+++ src/main/java/dev/aliaga/log/NumberLog.java
@@ -12,4 +12,21 @@ public class NumberLog {
 
         return accum == number;
     }
+
+    public static boolean friendsNumbers(int number1, int number2) {
+        int accum1 = 0;
+        int accum2 = 0;
+
+        for (int i = 1; i <= number1/2; i++) {
+            if (number1 % i == 0)
+                accum1 += i;
+        }
+
+        for (int i = 1; i <= number2/2; i++) {
+            if (number2 % i == 0)
+                accum2 += i;
+        }
+
+        return (number1 == accum2) && (number2 == accum1);
+    }
 }
